b7c12d241296de6472eaed9b8ee6c72af3bc631d
Alex <cigan@ua.fm>
2021-04-08 18:29:22 +0300

    feat: get the display and all buttons from document

---
+++ script.js
@@ -0,0 +1,6 @@
+const display = document.getElementById('display');
+const resetBtn = document.getElementById('reset');
+const dotSign = document.getElementById('dot');
+const equalSign = document.getElementById('equal');
+const operationSignsBtns = document.querySelectorAll('#operation');
+const nuberBtns = document.querySelectorAll('#btn');
